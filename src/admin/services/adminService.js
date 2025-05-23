abe20c2d75702f292835ad7efb2741f984f05fc3
Muhammad Usman Zafar <muhammadusmanzafar763@gmail.com>
2025-05-23 21:53:31 +0500

    Refactor getUsersList to return an empty array instead of throwing an error when no users are found

--- src/admin/services/adminService.js
+++ src/admin/services/adminService.js
@@ -31,7 +31,7 @@ exports.getUsersList = async (req, res) => {
         const totalUsers = await User.countDocuments(filters);
 
         if (!users || users.length === 0) {
-            throw new ApiError("No users found", httpStatus.status.NOT_FOUND);
+            return users || [];
         }
 
         return {

1b4697d46fe12b57ef485de2b20f65949f590a3a
Muhammad Usman Zafar <muhammadusmanzafar763@gmail.com>
2025-05-27 19:55:30 +0500

    Add post reports route to community management

--- src/admin/routes/index.js
+++ src/admin/routes/index.js
@@ -6,10 +6,11 @@ const adminDashboardRoutes = require('./adminDashboardRouter');
 const adminCommunityRoutes = require('./adminCommunityRouter');
 const adminComMemberRoutes = require('./adminComMemberRouter');
 const adminComPostRoutes = require('./adminComPostRouter');
+const adminPostReports = require('./adminPostReportsRouter');
 
 app.use('/user-management', adminRoutes);
 app.use('/dashboard', adminDashboardRoutes);
-app.use('/community-management', adminCommunityRoutes, adminComMemberRoutes, adminComPostRoutes);
+app.use('/community-management', adminCommunityRoutes, adminComMemberRoutes, adminComPostRoutes, adminPostReports);
 
 
 module.exports = app;
